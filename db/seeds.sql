fb102a1efb660fe808c1758e17094a2bf4c8a5f8
Ryan Gartner <ryanjgartner@gmail.com>
2019-03-17 11:28:37 -0500

    Beginning structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Royale with Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Jucy Lucy");
+INSERT INTO burgers (burger_name) VALUES ("Poutine on the Ritz Burger");
