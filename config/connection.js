64029782b3d24c312b23b8d3f29bfd652911f6a7
Ryan Gartner <ryanjgartner@gmail.com>
2019-03-28 20:59:36 -0500

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
+require('dotenv').config();
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "monkie1909",
+    password: process.env.DB_PW,
     database: "burgers_db"
-});
+})
+};
+
 
 connection.connect(function (err) {
     if (err) {
